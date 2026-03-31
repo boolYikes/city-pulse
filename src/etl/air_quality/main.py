7853f879527c6700822268c58e8667e1677c8f2e
Dongwon Henry Seon <123602432+boolYikes@users.noreply.github.com>
2026-03-31 06:47:22 +0000

    feat: add weather api and openaq ingest

--- src/etl/air_quality/main.py
+++ src/etl/air_quality/main.py
@@ -4,17 +4,17 @@ from etl.air_quality.ingest import run_openaq_ingestion
 def handler(event, context):
     lat = event["lat"]
     lon = event["lon"]
-    run_openaq_ingestion(lat, lon)
+    city = event["city"]
+    rad = event.get("rad", 12000)
+    ts = event.get("ts", None)
+    run_openaq_ingestion(city, lat, lon, rad, ts)
 
 
 # for dev
 if __name__ == "__main__":
-    from etl.common import Config
-
     # Central Park
     LAT = 40.769804
     LON = -73.974817
-    event = {"lat": LAT, "lon": LON}
-    config = Config()
+    event = {"lat": LAT, "lon": LON, "city": "New York", "rad": 12000, "ts": None}
 
     handler(event, {})
